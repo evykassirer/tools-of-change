5c110fb9f867e45e21c4db20433cd91a515168a8
evykassirer <evy.kassirer@gmail.com>
2024-02-29 16:22:35 -0800

    initial commit to scrape case studies

---
+++ create_backup.py
@@ -0,0 +1,36 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+# TODO
+# - styling for case study pages
+# - homepage
+
+
+def generate_case_study_page(f, url):
+  page = requests.get("https://toolsofchange.com/" + url)
+  f.write(page.text)
+
+
+def main():
+  URL = "https://toolsofchange.com/en/case-studies/?max=1000"
+  page = requests.get(URL)
+
+  soup = BeautifulSoup(page.content, "html.parser")
+
+  headers = soup.find_all(class_="sub_header_2col")
+
+  urls = [
+    header.find("a", href=True)['href']
+    for header in headers
+  ]
+
+  for url in urls:
+    if url[0] == "/":
+      url = url[1:]
+    if not os.path.exists(url):
+        os.makedirs(url)
+    with open(url + "index.html", "x") as f:
+      generate_case_study_page(f, url)
+
+main()
