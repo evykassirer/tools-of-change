a225705a91b4b3595c6e89180cb21d45bc541ad6
Evy Kassirer <evykassirer@users.noreply.github.com>
2025-10-18 13:19:02 -0700

    search: auto toggle case study or topic resource if coming from that part of the website (still broken for french topic resources)

--- scripts/search.js
+++ scripts/search.js
@@ -9,18 +9,39 @@ function searchButtonClick() {
   const search_value = document.getElementById("search_box").value;
   let search_url;
   let base_url;
-  if (window.location.href.includes("evykassirer.github.io")) {
+
+  const current_href = window.location.href;
+
+  if (current_href.includes("evykassirer.github.io")) {
     base_url = "https://evykassirer.github.io/tools-of-change";
   } else {
     base_url = window.location.origin;
   }
 
-  if (window.location.href.includes("/en/")) {
-    search_url = base_url + "/en/search?query=";
+  if (current_href.includes("/en/")) {
+    search_url = base_url + "/en/search";
   } else {
-    search_url = base_url + "/fr/recherche?query=";
+    search_url = base_url + "/fr/recherche";
   }
+
+  params = new URLSearchParams();
   if (search_value) {
-    window.location.href = search_url + search_value;
+    params.append("query", search_value);
   }
+
+  if (current_href.includes("en/case-studies") || current_href.includes("fr/etudes-de-cas")) {
+      params.append("type", "case-study");
+  }
+
+  // NOTE: french topic resources don't currently have the box set up for them
+  // but I can probably add it by it being part of the path?
+  if (current_href.includes("en/topic-resources") || current_href.includes("fr/ressources-de-sujets")) {
+      params.append("type", "topic-resource");
+  }
+
+  if (params.size > 0) {
+    search_url = search_url + "?" + params.toString();
+  }
+
+  window.location.href = search_url;
 };
