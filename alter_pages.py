f150d47c7865bad032110ab05d48386f51e01cb3
Evy Kassirer <evykassirer@users.noreply.github.com>
2025-10-22 18:32:47 -0700

    add more resource types for topic resources

--- alter_pages.py
+++ alter_pages.py
@@ -87,33 +87,48 @@ def alter_page(url):
 
     queue.append(full_url)
 
-  # change_made = False
-  # for img in soup.find_all('img'):
-  #   src = img['src']
-  #   if src.startswith("/images"):
-  #     img['src'] = url_relative_path_to_home(url) + src
-  #     print(url, img['src'])
-  #     change_made = True
+  change_made = False
+  for img in soup.find_all('img'):
+    src = img['src']
+    if src.startswith("/images"):
+      img['src'] = url_relative_path_to_home(url) + src
+      print("fixing image source")
+      change_made = True
 
-  if '<span data-pagefind-filter="Resource type' in str(soup):
-    return
+  if '<span data-pagefind-filter="Resource type' not in str(soup):
+    resource_type = None
+    if "topic-resources/detail" in url or "ressources-de-sujets/detail" in url:
+      resource_type = "Topic Resource"
+    elif "case-studies/detail" in url or "etudes-de-cas/detail" in url:
+      resource_type = "Case Study"
+    if resource_type:
+      new_tag = soup.new_tag("span")
+      new_tag['style'] = "display: none;"
+      new_tag['data-pagefind-filter'] = f"Resource type: {resource_type}"
+      soup.find("body").append(new_tag)
+      print("adding basic resource type")
+      change_made = True
+
 
   if "topic-resources/detail" in url or "ressources-de-sujets/detail" in url:
-    resource_type = "Topic Resource"
-  elif "case-studies/detail" in url or "etudes-de-cas/detail" in url:
-    resource_type = "Case Study"
-  else:
-    return
+    for tr in soup.find_all('tr'):
+      if tr.find(class_='highlight_box') and tr.find(class_='highlight_box').text in ["Catégorie:", "Resource Type:"]:
+        for resource_type in tr.find(class_='rt_text_box').text.split(","):
+          if resource_type.strip().lower() == "":
+            continue
+          full_resource_type = f"Resource type: Topic Resource: {resource_type.strip().lower()}"
+          if f'<span data-pagefind-filter="{full_resource_type}' not in str(soup):
+            new_tag = soup.new_tag("span")
+            new_tag['style'] = "display: none;"
+            new_tag['data-pagefind-filter'] = full_resource_type
+            soup.find("body").append(new_tag)
+            # print("adding advanced resource type")
+            change_made = True
 
-  new_tag = soup.new_tag("span")
-  new_tag['style'] = "display: none;"
-  new_tag['data-pagefind-filter'] = f"Resource type: {resource_type}"
-  soup.find("body").append(new_tag)
 
-
-  # if change_made:
-  with open(relative_url(url) + "index.html", "w") as f:
-    f.write(str(soup))
+  if change_made:
+    with open(relative_url(url) + "index.html", "w") as f:
+      f.write(str(soup))
 
 
 
